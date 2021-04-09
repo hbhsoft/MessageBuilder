a6453618dc7ff62abfc95ab7fcec3c95e7501388
hbhsoft <56706925+hbhsoft@users.noreply.github.com>
2021-04-09 09:25:46 +0700

    Add files via upload

---
+++ MainForm.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tphuy
+ * Date: 4/8/2021
+ * Time: 11:15 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DemoMessageBuilder
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			if(radioButton1.Checked)
+				textBox2.Text = HBHSOFT.MyMessageBuilderAPI.Encrypt(textBox1.Text);
+			else
+				textBox2.Text = HBHSOFT.MyMessageBuilderAPI.Decrypt(textBox1.Text);
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			Clipboard.Clear();Clipboard.SetText(textBox2.Text);
+		}
+	}
+}
